f1ca4c531157b09408111a0d99b4dafcbd4b988f
Dawei Dai <shachopin@gmail.com>
2017-04-25 14:34:48 -0700

    added comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def display_datetime(dt)
     dt.strftime("%m/%d/%Y %l:%M%P %Z") # 03/14/2013 9:09pm
   end
+
+  def preserve_linebreak(text)
+    text.gsub("\r\n", "<br />").html_safe
+  end
 end
