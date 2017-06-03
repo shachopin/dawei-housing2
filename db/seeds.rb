62a837af9eaf6d22918a32515263c56e1ced1434
shachopin <shachopin@gmail.com>
2017-06-03 02:35:39 +0000

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.create(username: 'shachopin')
+post1 = Post.create(title: 'A great company', url: 'oracle.com', description: 'Software and Hardware working together', creator: user1)
+Comment.create(body: "I agree", creator: user1, post: post1)
+category1 = Category.create(name: "Tech")
+PostCategory.create(category: category1, post: post1)
+
